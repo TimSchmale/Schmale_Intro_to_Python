86ae58bb4bc11b66bb76106c6d14eaec12cff7ca
TimSchmale <tim.schmale@tu-dortmund.de>
2025-09-29 21:53:12 +0200

    add init function

---
+++ src/football_stats_analysis/__init__.py
@@ -0,0 +1,13 @@
+"""
+football_stats_analysis package
+--------------------------------
+Provides tools for loading, preprocessing, analyzing, and visualizing
+European football data from the top five leagues (2014/15 – 2021/22).
+"""
+
+# Import main classes/functions for easy access
+from .DataHandler import DataHandler
+from .StatsCalculator import StatsCalculator
+from .Visualizer import Visualizer
+
+__version__ = "1.0.0"
